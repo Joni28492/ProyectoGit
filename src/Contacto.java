a1c7bab9ee83b32efad2035a7cdab747914453c5
Joni28492 <joni28492@gmail.com>
2018-10-11 12:53:44 +0200

    Subir al Repositorio la clase Contacto

---
+++ src/Contacto.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alumno36
+ */
+public class Contacto {
+    
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+
+    
+    
+}
