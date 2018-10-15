5acfda644be8edf857fff2407e05d960f19778eb
Joni28492 <joni28492@gmail.com>
2018-10-15 12:52:47 +0200

    se cambio la clase contacto al paquete net.intergrupo.contacto

--- src/PruebaContacto.java
+++ src/PruebaContacto.java
@@ -1,3 +1,6 @@
+
+import net.intergrupo.contacto.Contacto;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- src/net/intergrupo/contacto/Contacto.java
+++ src/net/intergrupo/contacto/Contacto.java
@@ -1,3 +1,5 @@
+package net.intergrupo.contacto;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
